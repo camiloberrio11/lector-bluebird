93c07b2c860423c852a8cd62795fca7e0da35b8f
camiloberrio <camiloberriouribe@gmail.com>
2021-11-10 10:05:35 -0500

    Repo library

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,10 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
-  constructor() {}
-
+  constructor() {
+    document.addEventListener('readystatechange', (event: any) =>
+      console.log('event1', event)
+    );
+    document.addEventListener('read', (event: any) =>
+      console.log('event2', event)
+    );
+    document.addEventListener('deviceready', (event: any) =>
+      console.log('Dispositivo listo', event)
+    );
+  }
 }
